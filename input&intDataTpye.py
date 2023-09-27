609d63063d0325e1bd0c4c6e3a333e9c8c339b64
noman-swe <noman.swe130@gmail.com>
2023-09-27 23:43:17 +0600

    input taking and int() usage and string and string methods

---
+++ input&intDataTpye.py
@@ -0,0 +1,9 @@
+
+
+name = input("Enter your name : ")
+print(type(name))
+
+#write a python program to take a number from user as an input from the user and add 6 to it
+age = input("Enter your age : ")
+print(type(age))
+print(int(age) + 6)
---
+++ stringDeepDive.py
@@ -0,0 +1,19 @@
+name = "Noman is a engineer"
+number = '5'
+print(name)
+
+'''Methods in Python'''
+#slice
+print(name[0:3]) #slicing 3 letters
+print(name[1:4]) #slicing index 1 to index 3 letters
+
+#String Methods
+print(name.upper()) #makes everything uppercase
+print(name.capitalize()) #makes 1st cherecter capitalize
+print(name.count("n")) #has to give a argument; and counts the argument(parameter) value; how much time the argument(perameter) value has in the string;
+print(name.isalnum())
+
+print(number.isnumeric()) #tells the string is numaric or not and gives result in boolean
+print(name.isnumeric()) #tells the string is numaric or not and gives result in boolean
+
+
