273bdbccd12cf39e08109b4835351bd1978447b1
noman-swe <noman.swe130@gmail.com>
2023-09-27 23:47:26 +0600

    Learn about Tuples and tupleMethods

---
+++ tuples&Methods.py
@@ -0,0 +1,13 @@
+t = (4, 6, 8, 10, 4)
+t1 = t.count(4)
+print(t1)
+
+
+
+
+'''Tuples is not editable and we can not change it and it is like e string'''
+#if we try to change tuples like list then we will have a error - TypeError: 'tuple' object does not support item assignment
+'''t[1] = 50
+print(t)
+so tuples can not be changed this is ummutable(oporibortoniyo)
+'''
