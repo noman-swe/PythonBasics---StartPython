ed7711140bb957cd5cd09155636f524894598745
noman-swe <noman.swe130@gmail.com>
2023-10-05 19:23:15 +0600

    slicing a string using string[startIndex:endIndex]

---
+++ All About Strings/strings.py
@@ -0,0 +1,9 @@
+cow = "The cow is a domestic animal"
+username = "nomanswe130"
+phone = "016881267722"
+
+# slicing a string
+slicingUserName = username[0:5]  # it will take index 0 to 4 & will stop taking before 5th index.
+slicingDeptName = username[5:8]  # it will take index 5 to 7 & will stop taking before 8th index.
+print("Name of the user : ", slicingUserName)
+print("Department of the user : ", slicingDeptName)
